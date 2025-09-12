a55ac146e292d7f846a70597ebb56adbd5f17acc
Francesco Palozzi <frapalozz@gmail.com>
2025-09-12 17:42:38 +0200

    Start board + some changes

--- main.c
+++ main.c
@@ -1,11 +1,14 @@
 #include <stdio.h>
-#include <stdbool.h>
 
+
+#define BLU   "\x1B[34m"
+#define WHT   "\x1B[37m"
+#define RESET "\x1B[0m"
 #define ROWS 8
 #define COLUMNS 8
 
 /* 
- * Chess Piece Struct 
+ * Chess Pieces 
  * =============================
  *           WHITE    BLACK
  * Pawn:       1        7
@@ -17,20 +20,56 @@
  * Empty:      0        0
  * =============================
  * */
-struct Piece {
-  unsigned char type;
-};
+char* getPiece(unsigned char piece) {
+  switch (piece) {
+    case 1: case 7:
+      return "P";
+    case 2: case 8:
+      return "N";
+    case 3: case 9:
+      return "B";
+    case 4: case 10:
+      return "R";
+    case 5: case 11:
+      return "Q";
+    case 6: case 12:
+      return "K";
+    default:
+      return 0;
+  }
+  /* TODO: test switch case */
+
+  if (piece == 1 || piece == 7)  return "P";
+  if (piece == 2 || piece == 8)  return "N";
+  if (piece == 3 || piece == 9)  return "B";
+  if (piece == 4 || piece == 10) return "R";
+  if (piece == 5 || piece == 11) return "Q";
+  if (piece == 6 || piece == 12) return "K";
+  return "\0";
+}
 
 /* Chess Board ROWSxCOLUMNS */
 struct Board {
-  struct Piece *board[ROWS][COLUMNS];
+  unsigned char board[ROWS][COLUMNS];
 };
+
+struct Board baseBoard = {10,8 ,9 ,11,12,9 ,8 ,10,
+                          7 ,7 ,7 ,7 ,7 ,7 ,7 ,7 ,
+                          0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,
+                          0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,
+                          0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,
+                          0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,
+                          1 ,1 ,1 ,1 ,1 ,1 ,1 ,1 ,
+                          4 ,2 ,3 ,5 ,6 ,3 ,2 ,4 };
 
 /* Print chess board to terminal */
 void printBoard(struct Board *board) {
   for(int i = 0; i < ROWS; i++) {
     for(int j = 0; j < COLUMNS; j++) {
-      printf("%d  ", board->board[i][j]->type);
+      if(board->board[i][j] < 7) 
+        printf(WHT "%s " RESET, getPiece(board->board[i][j]));
+      else 
+        printf(BLU "%s " RESET, getPiece(board->board[i][j]));
     };
     printf("\n");
   };
@@ -38,12 +77,9 @@ void printBoard(struct Board *board) {
 
 /* Reset board pieces */
 void resetBoard(struct Board *board) {
-  for(int i = 0; i < ROWS; i++) {
-    for(int j = 0; j < COLUMNS; j++) {
-      struct Piece pezzo = {0};
-      board->board[i][j] = &pezzo;
-    };
-  };
+  for(int i = 0; i < ROWS; i++) 
+    for(int j = 0; j < COLUMNS; j++) 
+      board->board[i][j] = baseBoard.board[i][j]; 
 }
 
 int main() 
@@ -53,7 +89,6 @@ int main()
 
   resetBoard(&board);
   printBoard(&board);
-  printf("Piece: %d Bytes\n", sizeof(struct Piece));
   printf("Board: %d Bytes\n", sizeof(struct Board));
 
   return 0;
